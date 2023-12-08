258d1bfec0e1402d4dbb46aa7f3ed9ca84ecd908
brendanwallison <57687858+brendanwallison@users.noreply.github.com>
2023-12-07 21:31:12 -0600

    Update setup-dirs.bash

--- setup-dirs.bash
+++ setup-dirs.bash
@@ -2,12 +2,12 @@
 echo "This script creates needed directories for collecting data through a BME280 Sensor and I2C Soil Moisture Sensors"
 echo ""
 echo "Setting up directories"
-sudo mkdir /DATA
-sudo chown $USER /DATA
-sudo chmod +x /DATA
-mkdir /DATA/sound
-mkdir /DATA/environmental
-mkdir /DATA/logs
+sudo mkdir ~/DATA
+sudo chown $USER ~/DATA
+sudo chmod +x ~/DATA
+mkdir ~/DATA/sound
+mkdir ~/DATA/environmental
+mkdir ~/DATA/logs
 
 echo "Checking Directories in /DATA"
 ls /DATA

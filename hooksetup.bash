c3df23cd96d1a5e80e6dff2580b2e5f5d2401f5c
brendanwallison <57687858+brendanwallison@users.noreply.github.com>
2023-12-07 21:15:59 -0600

    Update hooksetup.bash

--- hooksetup.bash
+++ hooksetup.bash
@@ -36,33 +36,33 @@ sudo pip install Adafruit-SSD1306
 
 # Set Up Required Directories
 echo "Adding required directories"
-bash /home/pi/Hook-Deployment/setup-dirs.bash
+bash ./setup-dirs.bash
 
 # Making Files executable
-sudo chmod +x /home/pi/Hook-Deployment/display.py
-sudo chmod +x /home/pi/Hook-Deployment/sensor_collect.py
-sudo chmod +x /home/pi/Hook-Deployment/transfersensorfiles.bash
-sudo chmod +x /home/pi/Hook-Deployment/cleanup-all-transferedsensorfiles.bash
+sudo chmod +x ./display.py
+sudo chmod +x ./sensor_collect.py
+sudo chmod +x ./transfersensorfiles.bash
+sudo chmod +x ./cleanup-all-transferedsensorfiles.bash
 
-# Installing Python Modules for the MinIO tests
-echo "Installing Python Modules for the MinIO tests"
-cd upstream-rpi/stengl-minio-tests 
-bash minio-clientsetup.bash 
+# # Installing Python Modules for the MinIO tests
+# echo "Installing Python Modules for the MinIO tests"
+# cd upstream-rpi/stengl-minio-tests 
+# bash minio-clientsetup.bash 
 
-# Setting up the Sunset and Sunrise Recordings
-echo "Setting up the Sunrise Sunset Recording Schedule"
-rm -rf /home/pi/upstream
-ln -s /home/pi/upstream-rpi /home/pi/upstream
-mkdir /home/pi/upstream/sound 
-sudo cp /home/pi/upstream/crontabs.txt /var/spool/cron/crontabs/pi 
-sudo chmod 0600 /var/spool/cron/crontabs/pi 
-sudo chown pi /var/spool/cron/crontabs/pi 
-sudo systemctl restart cron 
-rm /home/pi/upstream-rpi/sunrise-sunset-times.txt
-mv /home/pi/Hook-Deployment/sunrise-sunset-times.txt /home/pi/upstream-rpi/sunrise-sunset-times.txt
+# # Setting up the Sunset and Sunrise Recordings
+# echo "Setting up the Sunrise Sunset Recording Schedule"
+# # rm -rf /home/pi/upstream
+# # ln -s /home/pi/upstream-rpi /home/pi/upstream
+# # mkdir /home/pi/upstream/sound 
+# sudo cp ./crontabs.txt /var/spool/cron/crontabs/pi 
+# sudo chmod 0600 /var/spool/cron/crontabs/pi 
+# sudo chown pi /var/spool/cron/crontabs/pi 
+# sudo systemctl restart cron 
+# rm /home/pi/upstream-rpi/sunrise-sunset-times.txt
+# mv /home/pi/Hook-Deployment/sunrise-sunset-times.txt /home/pi/upstream-rpi/sunrise-sunset-times.txt
 
-echo "Scheduling the Recordings..."
-bash /home/pi/upstream/upstream-atqcheck.bash 
+# echo "Scheduling the Recordings..."
+# bash /home/pi/upstream/upstream-atqcheck.bash 
 
 echo "All done!"
 
